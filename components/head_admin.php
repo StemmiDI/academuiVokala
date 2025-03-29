a0ca0c83e381ef2f603c715d8e481271afdc4cb1
Yuri Kuzmichev <2091god@mail.ru>
2025-03-29 14:50:55 +0500

    исправила ошибки

--- components/head_admin.php
+++ components/head_admin.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="UTF-8" />
+  <base href="/" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Школа Вокала</title>
   <link
@@ -12,10 +13,10 @@
   <link rel="stylesheet" href="../css/footer.css">
   <link rel="stylesheet" href="../css/admin.css" />
   <link rel="stylesheet" href="../css/admin/admin-course.css">
-  <link rel="stylesheet" href="../css/admin/admin-schedule.css">
-  <link rel="stylesheet" href="../css/admin/admin-reviews.css">
+  <!-- <link rel="stylesheet" href="../css/admin/admin-schedule.css"> -->
+  <!-- <link rel="stylesheet" href="../css/admin/admin-reviews.css"> -->
   <link rel="stylesheet" href="../css/admin/admin-subscription.css">
-  <link rel="stylesheet" href="../css/admin/admin-teachers.css">
+  <link rel="stylesheet" href="../css/admin/admin-teacher.css">
 </head>
 
 <body>
