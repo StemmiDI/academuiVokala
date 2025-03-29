29eacb4d2e31260294d9972e4d7f6f94e361fe77
Yuri Kuzmichev <2091god@mail.ru>
2025-03-29 10:54:46 +0500

    refactoring code

--- app/db.php
+++ app/db.php
@@ -1,12 +1,21 @@
 <?php
-$host = 'localhost';  // хост
-$dbname = 'voc_academ'; // название базы данных
-$username = 'root';  // имя пользователя
-$password = '';  // пароль
+// Подключение к базе данных
+require_once __DIR__ . '/../vendor/autoload.php'; // Подключаем Composer autoload
+
+use Dotenv\Dotenv;
+
+// Загружаем переменные окружения
+$dotenv = Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];  // хост
+$dbname = $_ENV['DB_NAME']; // название базы данных
+$username = $_ENV['DB_USER'];  // имя пользователя
+$password = $_ENV['DB_PASS'];  // пароль
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Ошибка подключения: " . $e->getMessage();
 }
